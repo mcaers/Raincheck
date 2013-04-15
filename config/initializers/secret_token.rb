e4e347bdae659069b008a2cdf18c8d406a3dbcea
Michelle Caers <mcaers@gmail.com>
2013-04-15 15:54:50 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raincheck::Application.config.secret_token = '86a6fcddf900c007dc8edd7c057a39689faa39f38ba161cb02338ed28b0cbaa1a238b97d8e0b6621f796718517cd05fa7b4d4ef9a9d9b9c3452002bbd79aebc4'
